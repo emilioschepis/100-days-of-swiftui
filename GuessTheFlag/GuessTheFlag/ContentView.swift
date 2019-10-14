270c45a7d5d60c5040e6ffcafc9fb599b6afaaa4
Emilio Schepis <emilio.schepis@gmail.com>
2019-10-14 07:38:51 +0200

    Complete day 21

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,26 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = [
+        "Estonia", "France", "Germany", "Ireland", "Italy",
+        "Nigeria", "Poland", "Russia", "Spain", "UK", "US"
+        ].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.gray, .black]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
-            Button(action: {
-                self.showingAlert.toggle()
-            }) {
-                HStack {
-                    Image(systemName: "exclamationmark.triangle.fill")
-                    Text("Tap me to alert!")
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                        .font(.largeTitle)
+                        .fontWeight(.black)
                 }
-                .foregroundColor(.red)
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.primary, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                    
+                }
+                Spacer()
             }
         }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("SwiftUI"), message: Text("[Day 20/100] #100DaysOfSwiftUI"), dismissButton: .default(Text("OK")))
-            
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+                })
         }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
     }
 }
 
