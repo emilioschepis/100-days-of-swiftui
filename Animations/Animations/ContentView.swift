c6fa131712c443057a19a76e0a943a6e80fe7f49
Emilio Schepis <emilio.schepis@gmail.com>
2019-11-06 07:11:20 +0100

    Complete day 44

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,41 +8,85 @@
 
 import SwiftUI
 
-struct CornerRotateModifier: ViewModifier {
-    let amount: Double
-    let anchor: UnitPoint
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 300
     
-    func body(content: Content) -> some View {
-        content.rotationEffect(.degrees(amount), anchor: anchor)
-            .clipped()
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(
+                        LinearGradient(gradient:
+                            Gradient(colors: [
+                                self.color(for: value, brightness: 1),
+                            ]), startPoint: .top, endPoint: .bottom),
+                        lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
-extension AnyTransition {
-    static var pivot: AnyTransition {
-        .modifier(
-            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
-            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+struct Flower: Shape {
+    // Distance of this petal from the center.
+    var petalOffset: Double = -20
+    
+    // Width of this petal.
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            // Rotate the shape by the current value of the loop.
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // Move the petal to be in the center of the view.
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // Create the path for this petal.
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            
+            // Apply the transforms to the original petal.
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
     }
 }
 
 struct ContentView: View {
-    @State private var isShowingRed = false
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
         VStack {
-            Button("Tap Me") {
-                withAnimation {
-                    self.isShowingRed.toggle()
-                }
+            ZStack {
+                ColorCyclingCircle(amount: colorCycle)
+               Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.white, style: FillStyle(eoFill: true))
             }
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -20...20).padding([.horizontal, .bottom])
             
-            if isShowingRed {
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(width: 200, height: 200)
-                    .transition(.pivot)
-            }
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100).padding(.horizontal)
         }
     }
 }
