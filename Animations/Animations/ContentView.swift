5fb5be8a5f05596dad11e8e312f2e829d5cf9d03
Emilio Schepis <emilio.schepis@gmail.com>
2019-11-08 07:35:37 +0100

    Complete day 46

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,6 +8,38 @@
 
 import SwiftUI
 
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+    var startPoint: UnitPoint = .top
+    var endPoint: UnitPoint = .bottom
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient:
+                        Gradient(colors: [
+                            self.color(for: value, brightness: 1),
+                            self.color(for: value, brightness: 0.5),
+                        ]), startPoint: self.startPoint, endPoint: self.endPoint), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ColorCyclingCircle: View {
     var amount = 0.0
     var steps = 300
@@ -36,6 +68,46 @@ struct ColorCyclingCircle: View {
         }
         
         return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct Arrow: Shape {
+    var animatableData: AnimatablePair<CGFloat, CGFloat> {
+        get { AnimatablePair(lineWidth, angle) }
+        set {
+            self.lineWidth = newValue.first
+            self.angle = newValue.second
+        }
+    }
+    
+    var lineWidth: CGFloat = 3
+    var angle: CGFloat = 0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let pointY = rect.minY + (rect.height / 3)
+        
+        let rblCorner = CGPoint(x: rect.midX - 30, y: rect.maxY)
+        let rbrCorner = CGPoint(x: rect.midX + 30, y: rect.maxY)
+        let rtrCorner = CGPoint(x: rect.midX + 30, y: pointY)
+        let rtlCorner = CGPoint(x: rect.midX - 30, y: pointY)
+        let tlCorner = CGPoint(x: rect.minX + 30, y: pointY)
+        let trCorner = CGPoint(x: rect.maxX - 30, y: pointY)
+        let ttCorner = CGPoint(x: rect.midX, y: rect.minY)
+        
+        path.move(to: rblCorner)
+        path.addLine(to: rbrCorner)
+        path.addLine(to: rtrCorner)
+        path.addLine(to: trCorner)
+        path.addLine(to: ttCorner)
+        path.addLine(to: tlCorner)
+        path.addLine(to: rtlCorner)
+        path.addLine(to: rblCorner)
+        
+        let strokedPath = path.strokedPath(.init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+        
+        return strokedPath
     }
 }
 
@@ -98,49 +170,43 @@ struct ContentView: View {
     @State private var amount: CGFloat = 0.5
     @State private var firstInset: CGFloat = 50
     @State private var secondInset: CGFloat = 50
+    @State private var width: CGFloat = 3
+    @State private var angle: Double = 0
+    @State private var startPoint: UnitPoint = .top
+    @State private var endPoint: UnitPoint = .bottom
     
     var body: some View {
-        VStack {
-            ZStack {
-                Trapezoid(firstInset: firstInset, secondInset: secondInset)
-                    .frame(width: 500 * amount, height: 100)
-                    .offset(x: -80)
-                    .foregroundColor(Color.red)
-                    .blendMode(.screen)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            self.firstInset = CGFloat.random(in: 10...90)
-                            self.secondInset = CGFloat.random(in: 10...90)
-                        }
+        VStack(spacing: 30) {
+            Arrow(lineWidth: width)
+                .stroke(Color.white, style: StrokeStyle(lineWidth: width, lineCap: .round, lineJoin: .round))
+                .foregroundColor(.white)
+                .rotationEffect(.degrees(angle), anchor: .center)
+                .frame(width: 300, height: 300)
+            
+            HStack(spacing: 30) {
+                Button(action: {
+                    withAnimation(.linear(duration: 0.5)) {
+                        self.width = CGFloat.random(in: 1...10)
+                    }
+                }) {
+                    Image(systemName: "arrow.left.and.right")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .frame(width: 30)
+                    
                 }
                 
-                Trapezoid(firstInset: firstInset, secondInset: secondInset)
-                    .frame(width: 500 * amount, height: 100)
-                    .foregroundColor(Color.green)
-                    .offset(x: 80)
-                    .blendMode(.screen)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            self.firstInset = CGFloat.random(in: 10...90)
-                            self.secondInset = CGFloat.random(in: 10...90)
-                        }
-                }
-                
-                Trapezoid(firstInset: firstInset, secondInset: secondInset)
-                    .frame(width: 500 * amount, height: 100)
-                    .foregroundColor(Color.blue)
-                    .blendMode(.screen)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            self.firstInset = CGFloat.random(in: 10...90)
-                            self.secondInset = CGFloat.random(in: 10...90)
-                        }
+                Button(action: {
+                    withAnimation(.spring()) {
+                        self.angle = Double.random(in: 0...360)
+                    }
+                }) {
+                    Image(systemName: "arrow.2.circlepath")
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .frame(width: 30)
                 }
             }
-            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.black)
