854374cde764b8b38d26fbde7f77e501e68553b0
Emilio Schepis <emilio.schepis@gmail.com>
2019-11-15 07:15:55 +0100

    Complete day 53

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Emilio Schepis on 15/11/2019.
+//  Copyright © 2019 Emilio Schepis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+
+    var body: some View {
+        VStack {
+            List {
+                ForEach(students, id: \.id) { student in
+                    Text(student.name ?? "Unknown")
+                }
+            }
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+
+                let student = Student(context: self.moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? self.moc.save()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
