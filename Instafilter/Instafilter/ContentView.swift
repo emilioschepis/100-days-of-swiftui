bfdee085da4dc6e4befb64ae8f10c1bca7197f53
Emilio Schepis <emilio.schepis@gmail.com>
2019-12-02 20:18:11 +0100

    Complete day 62

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Emilio Schepis on 02/12/2019.
+//  Copyright © 2019 Emilio Schepis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
+    
+    var body: some View {
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
+        }
+        .actionSheet(isPresented: $showingActionSheet) {
+            ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
+                .default(Text("Red")) { self.backgroundColor = .red },
+                .default(Text("Green")) { self.backgroundColor = .green },
+                .default(Text("Blue")) { self.backgroundColor = .blue },
+                .cancel()
+            ])
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
