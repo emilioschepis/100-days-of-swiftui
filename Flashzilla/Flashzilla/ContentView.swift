dc2d2d8cd612f92aa73b61aae6bf616c735c2fa6
Emilio Schepis <emilio.schepis@gmail.com>
2019-12-25 10:55:41 +0100

    Complete day 86

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,93 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Emilio Schepis on 25/12/2019.
+//  Copyright © 2019 Emilio Schepis. All rights reserved.
+//
+
+import SwiftUI
+import CoreHaptics
+
+struct ContentView: View {
+    // how far the circle has been dragged
+    @State private var offset = CGSize.zero
+
+    // where it is currently being dragged or not
+    @State private var isDragging = false
+    
+    @State private var engine: CHHapticEngine?
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            return
+        }
+
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+
+    var body: some View {
+        // a drag gesture that updates offset and isDragging as it moves around
+        let dragGesture = DragGesture()
+            .onChanged { value in self.offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    self.offset = .zero
+                    self.isDragging = false
+                    self.complexSuccess()
+                }
+            }
+
+        // a long press gesture that enables isDragging
+        let tapGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    self.isDragging = true
+                }
+            }
+
+        // a combined gesture that forces the user to long press then drag
+        let combined = tapGesture.sequenced(before: dragGesture)
+
+        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
+        return Circle()
+            .fill(Color.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+            .onAppear(perform: prepareHaptics)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
